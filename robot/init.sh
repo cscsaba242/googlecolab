b26c978960b7e2d57946cd438b2cb114bf660790
Csaba Csikós <csaba-csikos@t-systems.com>
2024-10-27 15:30:00 +0100

    robot aws

--- robot/init.sh
+++ robot/init.sh
@@ -2,6 +2,9 @@
 #sudo yum -y install git
 #git config --global user.email "cscsaba242@live.com"
 #git config --global user.name "Csaba AWS"
+python3 -m venv .
+source ./bin/activate
+python3 -m ensurepip --upgrade
 python3 pip install pybit
 python3 pip install --upgrade pybit
 python3 pip install pandas
