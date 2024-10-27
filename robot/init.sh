948783143f0fe0185d0569f10c5179db9e586f0c
Csaba Csikós <csaba-csikos@t-systems.com>
2024-10-27 15:34:30 +0100

    robot aws

--- robot/init.sh
+++ robot/init.sh
@@ -5,10 +5,10 @@
 python3 -m venv .
 source ./bin/activate
 python3 -m ensurepip --upgrade
-python3 pip install pybit
-python3 pip install --upgrade pybit
-python3 pip install pandas
-python3 pip install pandas-ta
-python3 pip install backtesting
-python3 pip install python-telegram-bot
-python3 pip install asyncio
+python3 -m pip install pybit
+python3 -m pip install --upgrade pybit
+python3 -m pip install pandas
+python3 -m pip install pandas-ta
+python3 -m pip install backtesting
+python3 -m pip install python-telegram-bot
+python3 -m pip install asyncio
