d155f2b41c1cb2fa004b86f5a19d9359c07e4951
Csaba Csikós <csaba-csikos@t-systems.com>
2024-10-31 12:28:00 +0100

    changes AWS

--- robot/robo.py
+++ robot/robo.py
@@ -105,7 +105,6 @@ def getPrices(categoryParam, symbolParam, limitParam, colsParam):
   lf=pandas.DataFrame()
   url = f"https://api-testnet.bybit.com/v5/market/mark-price-kline?category={categoryParam}&symbol={symbolParam}&interval=1&limit={limitParam}"
   resp=requests.request("GET", url, headers=headers, data=payload)
-  logger.debug(f"{resp.text=}")
 
   lf = pandas.DataFrame(resp["result"]["list"], columns=colsParam)
   lf['Date'] = pandas.to_datetime(lf['Date'], unit='ms')
