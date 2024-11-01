dbef925e25dcd2907b7b06efddf082496ad73fd9
Csaba Csikós <csaba-csikos@t-systems.com>
2024-11-01 16:33:33 +0100

    csaba aws

--- robot/robo.py
+++ robot/robo.py
@@ -17,7 +17,7 @@ import yaml
 
 
 # Load YAML configuration
-with open("./logging_config.yaml", "r") as file:
+with open("logging_config.yaml", "r") as file:
     config = yaml.safe_load(file)
 logging.config.dictConfig(config)
 logger = logging.getLogger(__name__)
