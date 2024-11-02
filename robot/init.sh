f75de35b458ca0fb4dc09225fb5322ad5ba2a165
Csaba Csikós <csaba-csikos@t-systems.com>
2024-11-02 22:25:56 +0100

    cscsaba aws

--- robot/init.sh
+++ robot/init.sh
@@ -12,3 +12,14 @@ python3 -m pip install backtesting
 python3 -m pip install python-telegram-bot
 python3 -m pip install asyncio
 yes | cp squeeze_pro.py /home/ec2-user/googlecolab/robot/lib64/python3.9/site-packages/pandas_ta/momentum
+
+#'2024-11-02 21:18:32,655 - INFO - loop - DF.size=1215'
+#/home/ec2-user/googlecolab/robot/robo.py:150: UserWarning: Some prices are larger than initial cash value. Note that fractional trading is not supported. If you want to trade Bitcoin, increase initial cash, or trade μBTC or satoshis instead (GH-134).
+#  backtest = Backtest(DF, SmaCross,cash=10000*comp, commission=.002,exclusive_orders=True)
+#/home/ec2-user/googlecolab/robot/robo.py:150: UserWarning: Data index is not datetime. Assuming simple periods, but `pd.DateTimeIndex` is advised.
+#  backtest = Backtest(DF, SmaCross,cash=10000*comp, commission=.002,exclusive_orders=True)
+#/home/ec2-user/googlecolab/robot/lib64/python3.9/site-packages/backtesting/backtesting.py:1384: FutureWarning: The behavior of Series.idxmax with all-NA values, or any-NA and skipna=False, is deprecated. In a future version this will raise ValueError
+#  best_params = heatmap.idxmax()
+#'2024-11-02 21:18:32,831 - INFO - loop - optStrategy.equity=1000000, optStrategy.orders=(), optStrategy.position=<Position: 0 (0 trades)>, optStrategy.trades=()'
+#'2024-11-02 21:18:32,831 - INFO - loop - optStrategy.sma_f=5, optStrategy.sma_s=10'
+#^CTraceback (most recent call last):
