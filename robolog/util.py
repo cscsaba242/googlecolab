46a5eade0abf56e537bc250c3c38c1e105ee8be5
Csaba Csikós <csaba-csikos@t-systems.com>
2024-11-12 13:55:53 +0000

    setting logging

--- robolog/util.py
+++ robolog/util.py
@@ -1,11 +1,9 @@
 import logging.config
 import yaml
 
-
-# Load YAML configuration
-with open("./logging_config.yaml", "r") as file:
-    config = yaml.safe_load(file)
-
-# Apply logging configuration
-logging.config.dictConfig(config)
-logger = logging.getLogger(__name__)
+def get_logger(filename:str) -> logging.Logger:
+    with open("./logging_config.yaml", "r") as file:
+        config = yaml.safe_load(file)
+        config["handlers"]["timed_file"]["filename"] = filename
+        logging.config.dictConfig(config)
+        return logging.getLogger(__name__)
