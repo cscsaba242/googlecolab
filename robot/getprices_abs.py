cb5c1642f63f3b7b910035b8f6600470cdf7e19f
Csaba Colab <cscsaba242@live.com>
2024-11-22 18:05:50 +0000

    change

--- robot/getprices_abs.py
+++ robot/getprices_abs.py
@@ -10,12 +10,19 @@ DTStartEnd = namedtuple("DTStartEnd", ["start","end"])
 class GetPrices(ABC):
   timezone = 0 # utc
   logger = None
-  payload = {};
-  headers = {'User-Agent': 'Mozilla/5.0'}
+  payload = {}
+  headers = {}
+  start_time: datetime
 
-  def init(self, logger, timezone):
+  @abstractmethod
+  async def get_server_time() -> datetime:
+    pass
+
+  async def init(self, logger, timezone):
     self.logger = logger
     self.timezone = timezone 
+    start_time: datetime = await get_server_time()
+    logger.info(f"{start_time=}")
 
   @abstractmethod
   async def do(self, symbol:str, interval:str, start: datetime, end:datetime) -> DataFrame:
