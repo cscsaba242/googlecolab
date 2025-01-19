ee0cc69ba15ebd06c9362179b66aa04f6992f74d
cscsaba242 <cscsaba242@live.com>
2025-01-19 10:48:15 +0000

    changes

--- backtest/main.py
+++ backtest/main.py
@@ -3,13 +3,11 @@ import yfinance as yf
 import pandas as pa
 import numpy as np
 import ta
+import pdb
 
 symbol = yf.Ticker('MSFT')
 df = yf.download(['MSFT'], start='2025-01-01', interval='1h', auto_adjust=False)
-#print(df['Close'].shape)
-#print(type(df['Close']))
-#print(df.head())
-#df.xs('MSFT', axis=1, level='Ticker')
+
 df['stochrsi_k'] = ta.momentum.stochrsi_k(df['Close'].squeeze()) 
 df['stochrsi_d'] = ta.momentum.stochrsi_d(df['Close'].squeeze()) 
 for i in (8,14,50):
@@ -17,8 +15,7 @@ for i in (8,14,50):
 
 df['atr'] = ta.volatility.average_true_range(df['High'].squeeze(), df['Low'].squeeze(), df['Close'].squeeze())
 df.dropna(inplace=True)
-#print(symbol.info)
-#df = yf.download('EUROUSD', start='2022-01-01',interval='1h')
+
 def checkCross(data_frame):
     series = data_frame['stochrsi_k'] > data_frame['stochrsi_d']
     return series.diff()
@@ -27,30 +24,40 @@ df['cross'] = checkCross(df)
 df['TP'] = df['Close'].squeeze() + df['atr'] * 2
 df['SL'] = df['Close'].squeeze() - df['atr'] * 3
 
-df['Buysignal'] = np.where((df['cross'] == 1) & (df['Close'].squeeze() > df['EMA_8']) & (df['EMA_8'] > df['EMA_14']) & (df['EMA_14'] > df['EMA_50']), 1, 0)
+df['buy_signal'] = np.where((df['cross'] == 1) & (df['Close'].squeeze() > df['EMA_8']) & (df['EMA_8'] > df['EMA_14']) & (df['EMA_14'] > df['EMA_50']), 1, 0)
 
 selldates=[]
 outcome=[]
 
 for i in range(len(df)):
-    if df['Buysignal'].iloc[i]:
+    if df.buy_signal.iloc[i]:
         k = 1
-        SL = df['SL'].iloc[i]
-        TP = df['TP'].iloc[i]
-        inposition = True
-        while inposition:
-            looping_close = df['Close'].squeeze().iloc[i + k]
-            if looping_close >= TP:
+        SL = df.SL.iloc[i]
+        TP = df.TP.iloc[i]
+        in_position = True
+        while in_position:
+            if i + k >= len(df):
+                break
+            looping_high = df.High.iloc[i + k]
+            looping_low = df.Low.iloc[i+k]
+            if looping_high.squeeze() >= TP:
                 selldates.append(df.iloc[i + k].name)
                 outcome.append('TP')
-                inposition = False
-            elif looping_close <= SL:
+                in_position = False
+            elif looping_low.squeeze() <= SL:
                 selldates.append(df.iloc[i + k].name)
                 outcome.append('SL')
-                inposition = False
-            
-            k += 1
+                in_position = False
+            k +=1
+
+df.loc[selldates, 'sell_signal'] = 1
+df.loc[selldates, 'outcome'] = outcome
 
 
-print(df)
-
+df.sell_signal = df.sell_signal.fillna(0).astype(int)
+mask = df[(df.buy_signal == 1) | (df.sell_signal == 1) ]
+print(mask[['sell_signal','buy_signal','outcome']])
+#mask2 = mask[(mask.buy_signal.diff() == 1) | (mask.sell_signal.diff() == 1) ]
+#
+#
+#mask2.outcome.value_counts().sum()
